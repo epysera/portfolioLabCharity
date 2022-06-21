727539253cea17e34e6d42afd38e239298c7730e
twój login do GitHuba <twj email>
2022-06-21 20:32:47 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/charity/AppConfig.java
@@ -0,0 +1,27 @@
+package pl.coderslab.charity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableJpaRepositories(basePackages = "pl.coderslab.charity.repository")
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
+    }
+
+}
---
+++ src/main/java/pl/coderslab/charity/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package pl.coderslab.charity.repository;
+
+import pl.coderslab.charity.entity.Category;
+
+//public interface CategoryRepository extends JpaRespository <Category, Long> {
+//
+//
+//}
